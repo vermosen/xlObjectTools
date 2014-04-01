c0910962a5112331ab089541435884e92c707c7f
vermosen <vermosen.jm@free.fr>
2014-04-01 17:13:45 -0400

    cosmetic changes in xlInitiateIborIndex.hpp

--- xlFunction/object/index/xlInitiateIborIndex/xlInitiateIborIndex.hpp
+++ xlFunction/object/index/xlInitiateIborIndex/xlInitiateIborIndex.hpp
@@ -9,33 +9,20 @@
 #ifndef xl_initiate_ibor_index
 #define xl_initiate_ibor_index
 
-
 #include <ohxl/repositoryxl.hpp>
-
 #include <ohxl/conversions/validations.hpp>
-
 #include <ohxl/convert_oper.hpp>
-
 #include <ohxl/conversions/scalartooper.hpp>
 
-
 #include <qlo/indexes/iborindex.hpp>
-
 #include <qlo/valueobjects/vo_index.hpp>
 
-
 #include <bot/index/iborIndex/iborIndexObject.hpp>
-
 #include <bot/factory/period/periodFactory.hpp>
-
 #include <bot/factory/currency/currencyFactory.hpp>
-
 #include <bot/factory/calendar/calendarFactory.hpp>
-
 #include <bot/factory/businessDayConvention/businessDayConventionFactory.hpp>
-
 #include <bot/factory/daycounter/daycountFactory.hpp>
-
 #include <bot/tools/timeSeries/TimeSeriesObject.hpp>
 
         /* enregistre un index avec ses fixings */
@@ -48,7 +35,5 @@ DLLEXPORT char * xlInitiateIborIndex (const char * objectId_,
                                       const xloper * endOfMonth_,
                                       const char * dayCounter_,
                                       const xloper * trigger_) ;
-
-
 
 #endif
