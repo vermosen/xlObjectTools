1b69ca6bd73392f2ad4a5afac06ce71921d4ba0e
vermosen <vermosen.jm@free.fr>
2014-04-01 16:42:57 -0400

    fixed xlInitiateInterestRateBootstrapHelper function

--- xlFunction/object/curve/helpers/xlInitiateInterestRateSwapBootstrapHelper/xlInitiateInterestRateSwapBootstrapHelper.hpp
+++ xlFunction/object/curve/helpers/xlInitiateInterestRateSwapBootstrapHelper/xlInitiateInterestRateSwapBootstrapHelper.hpp
@@ -9,29 +9,20 @@
 #ifndef xl_initiate_interest_rate_swap_bootstrap_helper
 #define xl_initiate_interest_rate_swap_bootstrap_helper
 
-
 #include <ohxl/convert_oper.hpp>
-
 #include <ohxl/repositoryxl.hpp>
-
 #include <ohxl/conversions/validations.hpp>
-
 #include <ohxl/conversions/scalartooper.hpp>
 
-
 #include <bot/instruments/deposit/depositObject.hpp>
-
 #include <bot/curve/bootstrapHelper/depositBootstrapHelper/depositBootstrapHelperObject.hpp>
-
 #include <bot/factory/calendar/calendarFactory.hpp>
-
 #include <bot/factory/businessDayConvention/businessDayConventionFactory.hpp>
-
 #include <bot/curve/bootstrapHelper/interestRateSwapBootstrapHelper/interestRateSwapBootstrapHelperObject.hpp>
 
+#include <ql/instruments/vanillaSwap2.hpp>
 
 #include <qlo/termstructures.hpp>
-
 
         /* enregistre un helper pour un swap */
 DLLEXPORT xloper * xlInitiateInterestRateSwapBootstrapHelper (const char * objectID_,
@@ -39,6 +30,5 @@ DLLEXPORT xloper * xlInitiateInterestRateSwapBootstrapHelper (const char * objec
                                                               const double * quote_,
                                                               const xloper * discountCurve_,
                                                               const xloper * trigger_) ;
-
 
 #endif
