85391a491b7383e82cb56e14322578e1519055c9
vermosen <vermosen.jm@free.fr>
2014-05-06 16:15:07 -0400

    added test xlsx file

--- xlFunction/object/curve/helpers/xlInitiateDepositBootstrapHelper2/register_xlInitiateDepositBootstrapHelper2.hpp
+++ xlFunction/object/curve/helpers/xlInitiateDepositBootstrapHelper2/register_xlInitiateDepositBootstrapHelper2.hpp
@@ -14,50 +14,49 @@ inline void registerxlInitiateDepositBootstrapHelper2(const XLOPER & xDll) {
         Excel(xlfRegister, 0, 17, & xDll,
               TempStrNoSize("\x21""xlInitiateDepositBootstrapHelper2"),
               TempStrNoSize("\x09""PCCPPEPP#"),
-              TempStrNoSize("\x1D""INSTANCE.INTERPOLATION.DEPOT2"),
-              TempStrNoSize("\x69""Identifiant de l'objet,date de maturité,calendrier,jours de règlement,taux de dépôt,base annuelle,Trigger"),
+              TempStrNoSize("\x1C""OBJECT.CURVE.HELPER.DEPOSIT2"),
+              TempStrNoSize("\x50""object Id,maturity,calendar,settlement days,deposit rates,daycount basis,trigger"),
               TempStrNoSize("\x01""1"),
               TempStrNoSize("\x17""xlObjectTools - Object"),
               TempStrNoSize("\x00"""),
               TempStrNoSize("\x00"""),
-              TempStrNoSize("\x43""Cette fonction instancie un dépôt pour l'interpolation d'une courbe"),
-              TempStrNoSize("\x19""L'identifiant de l'object"),
-              TempStrNoSize("\x16""La date de maturité du dépôt"),
-			  TempStrNoSize("\x1A""Le calendrier de référence"),
-			  TempStrNoSize("\x1F""Le nombre de jours de règlement"),
-              TempStrNoSize("\x10""Le taux du dépôt"),
-			  TempStrNoSize("\x10""La base annuelle"),
-              TempStrNoSize("\x09""trigger  ")) ;
+              TempStrNoSize("\x3F""This function creates a deposit helper for yield curve building"),
+              TempStrNoSize("\x0D""The object Id"),
+              TempStrNoSize("\x1C""Maturity date of the deposit"),
+			  TempStrNoSize("\x12""Reference calendar"),
+			  TempStrNoSize("\x21""Number of business days to settle"),
+              TempStrNoSize("\x0C""Deposit rate"),
+			  TempStrNoSize("\x0E""Daycount basis"),
+              TempStrNoSize("\x09""trigger  "));
 
     }
 
-
 inline void unregisterxlInitiateDepositBootstrapHelper2(const XLOPER & xDll) {
 
-        XLOPER xlRegID ;
+        XLOPER xlRegID;
 
              // supprime la fonction xlInitiateDepositBootstrapHelper2
-        Excel(xlfRegister, 0, 17, & xDll,
-              TempStrNoSize("\x21""xlInitiateDepositBootstrapHelper2"),
-              TempStrNoSize("\x09""PCCPPEPP#"),
-              TempStrNoSize("\x1D""INSTANCE.INTERPOLATION.DEPOT2"),
-              TempStrNoSize("\x69""Identifiant de l'objet,ténor,calendrier,jours de règlement,taux de dépôt,base annuelle,Trigger"),
-              TempStrNoSize("\x01""1"),
-              TempStrNoSize("\x17""xlObjectTools - Object"),
-              TempStrNoSize("\x00"""),
-              TempStrNoSize("\x00"""),
-              TempStrNoSize("\x43""Cette fonction instancie un dépôt pour l'interpolation d'une courbe"),
-              TempStrNoSize("\x19""L'identifiant de l'object"),
-              TempStrNoSize("\x16""Le ténor du dépôt"),
-			  TempStrNoSize("\x1A""Le calendrier de référence"),
-			  TempStrNoSize("\x1F""Le nombre de jours de règlement"),
-              TempStrNoSize("\x10""Le taux du dépôt"),
-			  TempStrNoSize("\x10""La base annuelle"),
-              TempStrNoSize("\x09""trigger  ")) ;
+		Excel(xlfRegister, 0, 17, &xDll,
+			TempStrNoSize("\x21""xlInitiateDepositBootstrapHelper2"),
+			TempStrNoSize("\x09""PCCPPEPP#"),
+			TempStrNoSize("\x1C""OBJECT.CURVE.HELPER.DEPOSIT2"),
+			TempStrNoSize("\x50""object Id,maturity,calendar,settlement days,deposit rates,daycount basis,trigger"),
+			TempStrNoSize("\x01""1"),
+			TempStrNoSize("\x17""xlObjectTools - Object"),
+			TempStrNoSize("\x00"""),
+			TempStrNoSize("\x00"""),
+			TempStrNoSize("\x3F""This function creates a deposit helper for yield curve building"),
+			TempStrNoSize("\x0D""The object Id"),
+			TempStrNoSize("\x1C""Maturity date of the deposit"),
+			TempStrNoSize("\x12""Reference calendar"),
+			TempStrNoSize("\x21""Number of business days to settle"),
+			TempStrNoSize("\x0C""Deposit rate"),
+			TempStrNoSize("\x0E""Daycount basis"),
+			TempStrNoSize("\x09""trigger  "));
 
         Excel4(xlfRegisterId, & xlRegID, 2, & xDll,
             TempStrNoSize("\x21""xlInitiateDepositBootstrapHelper2")) ;
 
-        Excel4(xlfUnregister, 0, 1, & xlRegID) ;
+        Excel4(xlfUnregister, 0, 1, & xlRegID);
 
     }
